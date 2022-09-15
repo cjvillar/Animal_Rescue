9925077ed86b8991e15c2e7ab15bd47f8720f6f4
Chris Villarreal <chrisvillarreal84@gmail.com>
2022-09-15 16:14:16 -0700

    added components

---
+++ animal_rescue_frontend/src/Banner.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+export class Banner extends Component {
+    render() {
+      return(
+        <div className="banner">
+            <h1>ANIMAL RESCUE</h1>
+            <h3>Please Support Your Local Shelter</h3>
+        </div>
+             
+      )
+    }
+  
+  }
+  
+  export default Banner
