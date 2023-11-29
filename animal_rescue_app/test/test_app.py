b4d8db93fed1a1e1c3f45ada9cc4904b335445c1
Chris Villarreal <cjvillar@users.noreply.github.com>
2023-11-28 17:08:18 -0800

    test

--- animal_rescue_app/test/test_app.py
+++ animal_rescue_app/test/test_app.py
@@ -1,24 +1,24 @@
 import pytest
-from animal_rescue_api.views import Animal
-from animal_rescue_api.models import Animal
+# from animal_rescue_api.views import Animal
+# from animal_rescue_api.models import Animal
 
-@pytest.fixture
-def test_add_animal():
-    return Animal(
-    animal_type='Dog',
-    name='Max',
-    sex='Male',
-    breed='Golden Retriever',
-    neutered=True,
-    age=3,
-    weight='25 lbs',
-    adoption_status='AVAILABLE',
-    temperament='Friendly',
-    likes='Playing fetch',
-    fee='$150',
-    location='Shelter A',
-    image = 'no_image.jpg'
-)
+# @pytest.fixture
+# def test_add_animal():
+#     return Animal(
+#     animal_type='Dog',
+#     name='Max',
+#     sex='Male',
+#     breed='Golden Retriever',
+#     neutered=True,
+#     age=3,
+#     weight='25 lbs',
+#     adoption_status='AVAILABLE',
+#     temperament='Friendly',
+#     likes='Playing fetch',
+#     fee='$150',
+#     location='Shelter A',
+#     image = 'no_image.jpg'
+# )
 
 @pytest.mark.django_db
 def test_animal_rescue_api(client):
